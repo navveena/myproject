590953ef1185c58114048ddf0e5a9540034764e2
Naveena <navvna22@gmail.com>
2023-10-04 20:39:31 +0530

    added files

---
+++ accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from .forms import RegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            User.objects.create_user(username=username, email=email, password=password)
+            return redirect('login')  
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
